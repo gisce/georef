02e9577258813bc5426767c64af25e8112497472
Carles Pey <cpey@gisce.net>
2011-06-22 12:31:26 +0200

    NEW. Escollir nombre de processadors a georef_cini
    S'usa la variable d'entorn N_PROC per especificar el nombre de processos concurrents.
    cr: http://cr.gisce.net/6436152/

--- bin/georef_cini.py
+++ bin/georef_cini.py
@@ -6,12 +6,14 @@ from datetime import datetime
 import codecs
 import multiprocessing
 import sys
+import os
 
 from georef.loop import OOOP
 from progressbar import ProgressBar, ETA, Percentage, Bar
 
 sys.stdout = codecs.getwriter("utf-8")(sys.stdout)
-
+N_PROC = min(int(os.getenv('N_PROC', multiprocessing.cpu_count())),
+             multiprocessing.cpu_count())
 
 def producer(sequence, output_q):
     """Posem els items que serviran per fer l'informe.
@@ -59,7 +61,7 @@ def main():
     q = multiprocessing.JoinableQueue()
     q2 = multiprocessing.Queue()
     processes = [multiprocessing.Process(target=consumer, args=(q,q2))
-                 for x in range(0, multiprocessing.cpu_count())]
+                 for x in range(0, N_PROC)]
     processes += [multiprocessing.Process(target=progress,
                                               args=(len(sequence), q2))]
 

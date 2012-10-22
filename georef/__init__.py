41d02c223fdda2b535d970cc573932f2bf3a2bb8
Agustí Fita i Borrell <afita@gisce.net>
2012-10-22 18:18:13 +0200

    Bump to 0.12.5

--- georef/__init__.py
+++ georef/__init__.py
@@ -3,7 +3,7 @@ import sys
 
 from georef.codis_ine import CODIS_INE
 
-__version__ = '0.12.4'
+__version__ = '0.12.5'
 
 def get_codi_ine(ine):
     """Retorna el codi INE només si el que li passem té 3 caràcters a part de
